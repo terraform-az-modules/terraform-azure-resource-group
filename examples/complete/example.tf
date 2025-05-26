38a404132e8a02012da9aa20903266919b6b10c0
Rushi Mohite <110910146+rushimohite001@users.noreply.github.com>
2025-05-26 20:20:22 +0530

    feat: Added resource group terraform module code with updated changes.

--- examples/complete/example.tf
+++ examples/complete/example.tf
@@ -3,5 +3,18 @@ provider "azurerm" {
 }
 
 ##-----------------------------------------------------------------------------
-## Resources
+## Resource Group module call.
 ##-----------------------------------------------------------------------------
+module "resource_group" {
+  source      = "../../"
+  name        = "example"
+  environment = "test"
+  location    = "northeurope"
+  label_order = ["name", "environment", "location"] # locations logic are pre configured in the labels module
+  # custom_name             = "" # Optional: Overrides default naming logic with a fully custom name. Cannot be used if `name` is set.
+  resource_position_prefix = true # If true, resource type prefix (e.g., rg, rg-lock) is prepended to the name. Otherwise, it's appended.
+
+  #resource lock
+  resource_lock_enabled = true
+  lock_level            = "CanNotDelete"
+}
--- main.tf
+++ main.tf
@@ -1,3 +1,43 @@
 ##-----------------------------------------------------------------------------
-## Resources
+## Tagging Module – Applies standard tags to all resources
 ##-----------------------------------------------------------------------------
+module "labels" {
+  source          = "terraform-az-modules/tags/azure"
+  version         = "1.0.0"
+  name            = var.custom_name == null ? var.name : var.custom_name
+  location        = var.location
+  environment     = var.environment
+  managedby       = var.managedby
+  label_order     = var.label_order
+  repository      = var.repository
+  deployment_mode = var.deployment_mode
+  extra_tags      = var.extra_tags
+}
+
+##-----------------------------------------------------------------------------
+## Resource Group – Creates a resource group.
+##-----------------------------------------------------------------------------
+resource "azurerm_resource_group" "default" {
+  count    = var.enabled ? 1 : 0
+  name     = var.resource_position_prefix ? format("rg-%s", local.name) : format("%s-rg", local.name)
+  location = var.location
+  tags     = module.labels.tags
+
+  timeouts {
+    create = var.create
+    read   = var.read
+    update = var.update
+    delete = var.delete
+  }
+}
+
+##---------------------------------------------------------------------------------------------------------------
+## Management Lock - Manages a Management Lock which is scoped to a Subscription, Resource Group or Resource.
+##---------------------------------------------------------------------------------------------------------------
+resource "azurerm_management_lock" "resource-group-level" {
+  count      = var.enabled && var.resource_lock_enabled ? 1 : 0
+  name       = var.resource_position_prefix ? format("rg-lock-%s", local.name) : format("%s-rg-lock", local.name)
+  scope      = azurerm_resource_group.default[0].id
+  lock_level = var.lock_level
+  notes      = var.notes
+}
